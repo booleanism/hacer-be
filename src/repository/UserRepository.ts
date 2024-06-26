4119d58d392bf9b944a295f37ddaecf551e50529
LordAndree <andreasadrian46@gmail.com>
2024-06-26 12:37:18 +0700

    new

--- src/repository/UserRepository.ts
+++ src/repository/UserRepository.ts
@@ -46,7 +46,7 @@ export class UserRepository<C extends PoolClient>
     public async update<R extends WillBeRet<U>>(conn: C, data: U): Promise<R> {
         if (data.name && data.passwd && data.passwd && data.uname) {
             const query: Query = {
-                str: "UPDATE FROM Users SET name = $1, passwd = $2 WHERE uname = $3 RETURNING *",
+                str: "UPDATE Users SET name = $1, passwd = $2 WHERE uname = $3 RETURNING *",
                 args: [data.name, data.passwd, data.uname]
             };
 
